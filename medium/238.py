37b8db9541f9c28bd99d8bacd254645c6fb57f62
tankarageldi <mkarageldi@mta.ca>
2024-07-19 19:19:24 -0300

    product of array except self solution uploaded

--- medium/238.py
+++ medium/238.py
@@ -1,15 +1,30 @@
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
-        for i in range(len(nums)):
-            left_product = 
-            right_product = 
+        length = len(nums)
+        output = [None] * (length-1)
+        output.insert(0,1)
+        r = 1
+
+        for i in range(1,length):
+            output[i] = nums[i-1] * output[i-1]
+
+        for i in range(length-1,0,-1):
+            output[i-1] = output[i-1] * nums[i] * r
+            r = r * nums[i]
+            
+        return output
+
+            
+            
+
+            
     
             
 
 
 
 sol = Solution()
-nums1 = [1,2,3,4]
+nums1 = [1,2,3,4] # [24,12,8,6]
 nums2 = [-1,1,0,-3,3]
 print(sol.productExceptSelf(nums1))
 print(sol.productExceptSelf(nums2))
