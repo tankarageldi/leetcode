7ad20ebe7cb1f753a6676db5b29df2f311a2ff2a
tankarageldi <mkarageldi@mta.ca>
2024-08-03 17:39:31 -0300

    daily question

---
+++ easy/205.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        map1=[]
+        map2=[]
+
+        for i in s:
+            map1.append(s.index(i))
+        for i in t:
+            map2.append(t.index(i))
+        return map1 == map2
+
+            
+sol = Solution()
+s ="bbbaaaba"
+t ="aaabbbba"
+print(sol.isIsomorphic(s,t))
