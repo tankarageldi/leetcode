68fbb83799cbd99cdf8f635bf2bb9efe9d6e9104
tankarageldi <mkarageldi@mta.ca>
2024-10-21 21:17:49 -0300

    daily

---
+++ medium/2095.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = head
+        slow = head
+        prev = None
+        while fast and fast.next: 
+            fast = fast.next.next
+            prev = slow
+            slow = slow.next
+        if prev:
+            prev.next = slow.next
+            slow.next = None
+        else:
+            return None
+        return head
