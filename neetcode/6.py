27db69a5aaa544d3ccff99082f0223d0fce2d456
tankarageldi <mkarageldi@mta.ca>
2024-12-03 18:24:48 -0400

    daily neetcode

--- neetcode/6.py
+++ neetcode/6.py
@@ -1,22 +1,30 @@
 class Solution:
     def encode(self, strs: list[str]) -> str:
-        ans = ""
-        for i in range(len(strs)):
-            if i == (len(strs) - 1):
-                ans += strs[i]
-            else: 
-                ans += strs[i] + "!"
-        return ans
-        
+        res = ""
+        for x in strs:
+            res += str(len(x)) + "#" + x
+        return res
     def decode(self, s: str) -> list[str]:
-        if s == "":
-            return ""
-        if s == []:
-            return []
-        return s.split("!")
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            l = int(s[i:j])
+            res.append(s[j + 1 : j + 1 + l])
+            i = j + 1 + l
+        return res
+
+            
+
+
+
+       
 
 s = Solution()
 strs= ["neet","code","love","you"]
 x = s.encode(strs)
 print(x)
-print(s.decode(x))
+a = s.decode(x)
+print(a)
