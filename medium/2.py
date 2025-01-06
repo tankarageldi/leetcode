0f3732c96067549995a5ebf267215d5fad663250
tankarageldi <mkarageldi@mta.ca>
2025-01-06 15:20:41 -0400

    question

--- medium/2.py
+++ medium/2.py
@@ -7,30 +7,46 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
+    # did it myslf
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        left = 0
-        right = 0
-        multi = 1
-        while l1 and l2:
-            left += (l1.val)*multi
-            l1=l1.next
-            right += (l2.val)*multi
-            l2=l2.next
-            multi = multi * 10
-        total = right + left
-        s = str(total)
-        ans = [int(digit) for digit in reversed(s)]
-        if not l1 or l2:
-            return ListNode(0)
-        i = 0
-        while l1 and i < len(ans): 
-            l1.val = ans[i]
-            i += 1
-        return l1
+        first = ''
+        second = ''
+        while l1:
+            first = str(l1.val) + first
+            l1 = l1.next
+        while l2:
+            second = str(l2.val) + second
+            l2 = l2.next
         
+        total = int(first) + int(second)
+        total = total
+        dummy = ListNode()
+        curr = dummy
+        for digit in str(total)[::-1]:
+            curr.next = ListNode(int(digit))
+            curr = curr.next
         
+        return dummy.next
+        
+     ## optimal solution
+        
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummyHead = ListNode(0)
+        curr = dummyHead
+        carry = 0
+        while l1 != None or l2 != None or carry != 0:
+            l1Val = l1.val if l1 else 0
+            l2Val = l2.val if l2 else 0
+            columnSum = l1Val + l2Val + carry
+            carry = columnSum // 10
+            newNode = ListNode(columnSum % 10)
+            curr.next = newNode
+            curr = newNode
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummyHead.next
 
-l1 = [2,4,3]
-l2 = [5,6,4]
-sol = Solution()
-print(sol.addTwoNumbers(l1,l2))
+        
+        
+        
+        
