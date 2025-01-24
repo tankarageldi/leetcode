9bb5062c8b9595d276d697ed0de0e1f293066435
tankarageldi <mkarageldi@mta.ca>
2025-01-24 19:50:57 -0400

    daily q

---
+++ medium/802.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        safe = {}
+
+        def dfs(i):
+            if i in safe:
+                return safe
+            safe[i] = False
+            for nei in graph[i]:
+                if not dfs(nei):
+                    return safe[i]
+            safe[i] = True
+            return safe[i]
+
+        res = []
+        for i in range(n):
+            if dfs(i):
+                res.append(i)
+        return res
